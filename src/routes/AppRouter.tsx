77c58cd168f8132a0d90db355163f22612e12362
Josh Muleshi <jmuleshi2@gmail.com>
2025-03-24 14:04:38 -0400

    done

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,15 +1,11 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from '../pages/Home';
 import { MarkdownEditor } from "../pages/MarkdownEditor";
-import PrivateRoute from "./PrivateRouter";
+//import PrivateRoute from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <div className="p-4 text-center">
-        <nav className="mb-4">
-          <Link to="/" className="mr-4 text-blue-500">Home</Link>
-          <Link to="/markdown" className="text-blue-500">Markdown-Editor</Link>
-        </nav>
 
         <Router>
             <Routes>
