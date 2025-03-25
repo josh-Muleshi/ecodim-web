eeb99a9cda4bccd3f4689bfb3f0622bcfa2ecaf7
Josh Muleshi <jmuleshi2@gmail.com>
2025-03-25 07:44:12 -0400

    footer and navbar added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-800 text-white text-center p-3">
+        <p>© {new Date().getFullYear()} Ecodim. Tous droits réservés.</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,31 @@
+import { Link, NavigateFunction, useNavigate } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "../services/firebase";
+
+const Navbar = () => {
+  const navigate: NavigateFunction = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion :", error);
+    }
+  };
+
+  return (
+    <nav className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">Ecodim</h1>
+      <div className="space-x-4">
+        <Link to="/dashboard" className="hover:underline">Accueil</Link>
+        <Link to="/dashboard/postItem" className="hover:underline">Ajouter une Lecon</Link>
+        <button onClick={handleLogout} className="hover:underline bg-transparent border-none text-white cursor-pointer">
+          Déconnexion
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
